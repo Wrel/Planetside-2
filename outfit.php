9594ff3e8d787a589b44c0e8245b8e6050481717
cmd430 <cmd430@gmail.com>
2015-04-08 15:06:39 +1200

    fix for SOE becoming DBG

--- outfit.php
+++ outfit.php
@@ -2,8 +2,8 @@
 	//API Service ID
 	$sid = "<YOUR SERVICE ID>"; //http://census.soe.com/#service-id
 	//Pull Census API Data
-	$outfitID = json_decode(file_get_contents("http://census.soe.com/s:".$sid."/get/ps2/outfit/?alias=".$_GET['tag']."&c:show=outfit_id,name,member_count"), true);
-	$Census = file_get_contents("http://census.soe.com/s:".$sid."/get/ps2/outfit_member?c:limit=1000&c:resolve=online_status,character(name.first)&c:hide=member_since,member_since_date,outfit_id,rank,rank_ordinal&outfit_id=".$outfitID['outfit_list'][0]['outfit_id']);
+	$outfitID = json_decode(file_get_contents("http://census.daybreakgames.com/s:".$sid."/get/ps2/outfit/?alias=".$_GET['tag']."&c:show=outfit_id,name,member_count"), true);
+	$Census = file_get_contents("http://census.daybreakgames.com/s:".$sid."/get/ps2/outfit_member?c:limit=1000&c:resolve=online_status,character(name.first)&c:hide=member_since,member_since_date,outfit_id,rank,rank_ordinal&outfit_id=".$outfitID['outfit_list'][0]['outfit_id']);
 	$outfitData = json_decode($Census, true);
 	//Return JSON, To Be added by Below Code.
 	$outfitInfo = array();
